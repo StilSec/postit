a5fec40dc2d072f609f02360d5080b719079e604
StilSec <samsonf424@gmail.com>
2026-09-25 17:00:15 +0800

    fixed post card height issue

--- src/app/(dashboard)/posts/Dashboard.tsx
+++ src/app/(dashboard)/posts/Dashboard.tsx
@@ -43,7 +43,7 @@ export default function Dashboard({
       </div>
 
       {(posts.length > 0) ? (
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2 items-start">
           {posts.map((post) => (
             <PostCard
               key={post.post_id}
